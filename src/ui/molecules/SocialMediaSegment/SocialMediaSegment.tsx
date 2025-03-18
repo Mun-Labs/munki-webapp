cccdfc136ff6db87d7f1adcc7c7aae3670fd66b2
Your Name <you@exmple.com>
2025-03-18 19:10:31 +0700

    ui: add footer

--- src/ui/molecules/SocialMediaSegment/SocialMediaSegment.tsx
+++ src/ui/molecules/SocialMediaSegment/SocialMediaSegment.tsx
@@ -2,7 +2,7 @@
 import { ComponentProps, FC } from "react";
 import styled from "styled-components";
 import { Icon } from "../../atoms/Icon/Icon";
-import { IIconMap, SocialKeys } from "../../../domain/types/Types";
+import { IIconMap, SocialMedia } from "../../../domain/types/Types";
 
 const SocialMediaSegmentStyled = styled.div.attrs({
   className: "SocialMediaSegmentStyled",
@@ -13,16 +13,25 @@ const SocialMediaSegmentStyled = styled.div.attrs({
 const iconMap: IIconMap = {
   website: { src: "/icons/website.svg" },
   web: { src: "/icons/website.svg" },
-  telegram: { src: "/icons/telegram.svg", height: 20 },
+  telegram: {
+    src: "/icons/telegram.svg",
+    width: 25,
+    height: 20,
+    style: { marginTop: 1 },
+  },
   x: { src: "/icons/x.svg" },
 };
 
 interface SocialMediaSegmentProps extends ComponentProps<any> {
-  socials?: SocialKeys[];
+  socials?: SocialMedia[];
 }
 
 const defaultSocialMediaSegmentProps: SocialMediaSegmentProps = {
-  socials: ["website", "telegram", "x"],
+  socials: [
+    { name: "Website", type: "website", url: "https://example.com" },
+    { name: "Telegram", type: "telegram", url: "https://t.me/example" },
+    { name: "X", type: "x", url: "https://x.com/example" },
+  ],
 };
 
 export const SocialMediaSegment: FC<SocialMediaSegmentProps> = (
@@ -36,13 +45,21 @@ export const SocialMediaSegment: FC<SocialMediaSegmentProps> = (
   return (
     <SocialMediaSegmentStyled style={{ ...style }}>
       {socials.map((social) => (
-        <Icon
-          key={social}
-          src={iconMap[social].src}
-          width={iconMap[social].width}
-          height={iconMap[social].height}
-          style={{ marginRight: 8 }}
-        />
+        <a
+          key={social.type}
+          href={social.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
+        >
+          <Icon
+            key={social.type}
+            src={iconMap[social.type].src}
+            width={iconMap[social.type].width}
+            height={iconMap[social.type].height}
+            style={{ marginRight: 8, ...iconMap[social.type].style }}
+          />
+        </a>
       ))}
     </SocialMediaSegmentStyled>
   );
--- src/ui/organisms/MunkiFooter/MunkiFooter.tsx
+++ src/ui/organisms/MunkiFooter/MunkiFooter.tsx
@@ -1,13 +1,67 @@
+import { Button, Flex, Row } from "antd";
 import "./MunkiFooter.css";
+import styled from "styled-components";
+import { SocialMediaSegment } from "../../molecules/SocialMediaSegment/SocialMediaSegment";
+import { SocialMedia } from "../../../domain/types/Types";
 
 const footerStyle: React.CSSProperties = {
-  height: "50px",
+  height: "84px",
+  borderTop: "1px solid #ccc",
+  background: "rgb(1, 1, 1)",
+  position: "relative",
+  zIndex: 10,
+  padding: "0 120px",
 };
+
+const FooterButton = styled(Button)`
+  color: white;
+  margin: 0 10px;
+  padding: unset;
+  font-size: 12px;
+`;
+
+const socials: SocialMedia[] = [
+  {
+    name: "Website",
+    type: "website",
+    url: "https://munki-ai.gitbook.io/munki-ai",
+  },
+  { name: "Telegram", type: "telegram", url: "https://t.me/elmunkibot" },
+  { name: "X", type: "x", url: "https://x.com/munki_ai" },
+];
 
 export const MunkiFooter = () => {
   return (
-    <section className="footer" style={footerStyle}>
-      Sticky Footer
-    </section>
+    <Flex className="footer" style={footerStyle} justify="space-between">
+      <div className="info">
+        <img
+          width="60"
+          height="60"
+          src="/munki-header.png"
+          alt="munki"
+          style={{
+            marginLeft: 15,
+            marginTop: 15,
+            marginBottom: -15,
+          }}
+        />
+        <Row>
+          <FooterButton type="link">Privacy Policy</FooterButton>
+          <FooterButton type="link">Terms of User</FooterButton>
+          <FooterButton type="link">Terms of Sale</FooterButton>
+          <FooterButton type="link">© 2025 MUN</FooterButton>
+        </Row>
+      </div>
+      <div
+        className="socials"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 10,
+        }}
+      >
+        <SocialMediaSegment socials={socials} style={{ gap: 20 }} />
+      </div>
+    </Flex>
   );
 };
