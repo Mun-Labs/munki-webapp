3c6e11a3b4b11c9514472a3409a71c8e007c1935
Your Name <you@exmple.com>
2025-03-24 16:48:53 +0700

    fix: prop naming for styled components

--- src/ui/organisms/MunkiSidebar/MunkiSidebar.tsx
+++ src/ui/organisms/MunkiSidebar/MunkiSidebar.tsx
@@ -8,14 +8,14 @@ export const MunkiSidebar = () => {
   const [isCollapse, setIsCollapse] = useState(false);
 
   return (
-    <MunkiSidebarStyled isCollapse={isCollapse}>
+    <MunkiSidebarStyled $isCollapse={isCollapse}>
       <p onClick={() => setIsCollapse(!isCollapse)}>Hahaha</p>
     </MunkiSidebarStyled>
   );
 };
 
 interface IMunkiSidebarStyled {
-  isCollapse: boolean;
+  $isCollapse: boolean;
 }
 
 const MunkiSidebarStyled = styled.div.attrs<IMunkiSidebarStyled>({
@@ -29,7 +29,7 @@ const MunkiSidebarStyled = styled.div.attrs<IMunkiSidebarStyled>({
   background: #000;
   color: ${COLORS.yellow};
   z-index: 100;
-  width: ${(props) => (props.isCollapse ? "1rem" : "15rem")};
+  width: ${(props) => (props.$isCollapse ? "1rem" : "15rem")};
   height: 100vh;
   padding: 20px;
   border-right: 1px solid ${COLORS.yellow};
