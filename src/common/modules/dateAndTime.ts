1ba743c31e278a2a94cd0b2b461c35a571b46051
Your Name <you@exmple.com>
2025-03-28 01:27:36 +0700

    dev(modules): short date

--- src/common/modules/dateAndTime.ts
+++ src/common/modules/dateAndTime.ts
@@ -1,5 +1,12 @@
 import { DateTime } from "luxon";
 
-export function unixToDate(unix: number): string {
-  return DateTime.fromSeconds(unix).toLocaleString(DateTime.DATE_MED);
+export function unixToDate(
+  unix: number,
+  format: "short" | "med" = "med",
+): string {
+  let finalFormat = DateTime.DATE_MED;
+  if (format === "short") {
+    finalFormat = { month: "numeric", day: "numeric", year: "2-digit" };
+  }
+  return DateTime.fromSeconds(unix).toLocaleString(finalFormat);
 }
