348ab345a00f81e871d050f096326c1805969151
Your Name <you@exmple.com>
2025-04-02 02:09:38 +0700

    ui: details: adjust scores

--- src/ui/pages/TokenDetailsPage/Analytics/SectionScore/SectionScore.tsx
+++ src/ui/pages/TokenDetailsPage/Analytics/SectionScore/SectionScore.tsx
@@ -23,32 +23,48 @@ export const SectionScore: FC<SectionScoreProps> = () => {
 
   return (
     <SectionScoreStyled>
-      <div className="mun-score wrap-score">
+      <section
+        className="mun-score wrap-score"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
         <h3>Mun score</h3>
 
-        <h4
-          style={{
-            marginTop: "32px",
-            color: COLORS.white60,
-            fontSize: "34px",
-          }}
-        >
-          Level 7: Alpha
-        </h4>
-        <h4
-          style={{
-            color: COLORS.chlorophyll_green,
-            fontSize: "78px",
-          }}
-        >
-          {munScore ?? "n/a"} PTS
-        </h4>
+        <div>
+          <h4
+            style={{
+              marginTop: "32px",
+              color: COLORS.white60,
+              fontSize: "34px",
+            }}
+          >
+            Level 7: Alpha
+          </h4>
+          <h4
+            style={{
+              color: COLORS.chlorophyll_green,
+              fontSize: "78px",
+            }}
+          >
+            {munScore ?? "n/a"} PTS
+          </h4>
 
-        <div style={{ width: "98%" }}>
-          <Slider marks={marks} value={103} />
+          <div style={{ width: "98%" }}>
+            <Slider marks={marks} value={103} />
+          </div>
         </div>
-      </div>
-      <div className="risk-score wrap-score">
+      </section>
+      <section
+        className="risk-score wrap-score"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
         <h3>Risk score</h3>
         <div
           style={{
@@ -62,11 +78,14 @@ export const SectionScore: FC<SectionScoreProps> = () => {
             value={103}
           />
         </div>
-        <h4 className="safe">Safe</h4>
-        <h4 className="safe-value">{103}</h4>
-      </div>
 
-      <div className="top-followers wrap-score">
+        <div>
+          <h4 className="safe">Safe</h4>
+          <h4 className="safe-value">{103}</h4>
+        </div>
+      </section>
+
+      <section className="top-followers wrap-score">
         <h3>Top Followers</h3>
 
         <div className="header">
@@ -78,68 +97,17 @@ export const SectionScore: FC<SectionScoreProps> = () => {
           .map((follower, index) => {
             return <TopFollowerItem key={follower.tag + index} {...follower} />;
           })}
-      </div>
+      </section>
     </SectionScoreStyled>
   );
 };
-
-const TopFollowersItemStyled = styled.div.attrs({
-  className: "TopFollowersItemStyled",
-})`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 10px;
-  margin: 10px 0;
-
-  .profile {
-    gap: 12px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .profile-img {
-      border-radius: 50%;
-    }
-    .profile-info {
-      .profile-info--xname {
-        /* font-family: Arial, Helvetica, sans-serif; */
-        color: ${COLORS.grey50};
-        font-size: 15px;
-      }
-      .profile-info--name {
-        /* font-family: Arial, Helvetica, sans-serif; */
-        font-weight: 600;
-        font-size: 20px;
-      }
-    }
-  }
-
-  .follower {
-    font-size: 38px;
-    font-weight: 600;
-    color: ${COLORS.grey50};
-  }
-
-  @media (max-width: 800px) {
-    .profile {
-      .profile-info {
-        .profile-info--name {
-          font-size: 16px;
-        }
-      }
-    }
-    .follower {
-      font-size: 24px;
-    }
-  }
-`;
 
 const SectionScoreStyled = styled.div.attrs({
   className: "SectionScoreStyled",
 })`
   border: 1px solid ${COLORS.magenta};
   display: grid;
-  grid-template-columns: 1.2fr 0.7fr 0.8fr;
+  grid-template-columns: 1.2fr 0.5fr 1fr;
   grid-template-areas: "munScore riskScore topFollowers";
 
   .mun-score {
@@ -161,16 +129,16 @@ const SectionScoreStyled = styled.div.attrs({
     }
   }
 
-  & > div:last-child {
+  & > section:last-child {
     border-right: none;
   }
-  & > div {
+  & > section {
     padding: 24px;
     border-right: 1px solid ${COLORS.magenta};
   }
 
   .wrap-score {
-    padding: 12px;
+    padding: 24px;
     h3 {
       font-size: 28px;
     }
