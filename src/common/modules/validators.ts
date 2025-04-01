fe22035271a77a005f91833783eae2dcbbaa7460
Your Name <you@exmple.com>
2025-04-01 23:38:35 +0700

    dev: remove logs

--- src/common/modules/validators.ts
+++ src/common/modules/validators.ts
@@ -4,9 +4,7 @@ import { PublicKey } from "@solana/web3.js";
 export function isSolanaAddress(address: Address): boolean {
   try {
     const pubKey = new PublicKey(address);
-    /*prettier-ignore*/ console.log('>>>> _ >>>> ~ validators.ts:7 ~ isSolanaAddress ~ pubKey:', pubKey)
     const is = PublicKey.isOnCurve(pubKey);
-    /*prettier-ignore*/ console.log('>>>> _ >>>> ~ validators.ts:9 ~ isSolanaAddress ~ is:', is)
     return is;
   } catch {
     return false;
